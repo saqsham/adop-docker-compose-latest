6d5c78c543a2a3c97353b0dbca35a7a5ee1f1ec9
saqsham <saqsham@gmail.com>
2020-10-06 04:09:56 +0530

    some-changes
    Signed-off-by: saqsham <saqsham@gmail.com>

--- env.config.sh
+++ env.config.sh
@@ -4,8 +4,8 @@
 # LDAP
 
 export LDAP_SERVER="ldap:389"
-export LDAP_DOMAIN="ldap.example.com"
-export LDAP_FULL_DOMAIN="dc=ldap,dc=example,dc=com"
+export LDAP_DOMAIN="ldap.xongl.com"
+export LDAP_FULL_DOMAIN="dc=ldap,dc=xongl,dc=com"
 export LDAP_USER_BASE_DN="ou=people"
 export LDAP_GROUP_BASE_DN="ou=groups"
 export LDAP_USER_SEARCH="uid={0}"
@@ -14,7 +14,7 @@ export LDAP_ACCOUNTPATTERN='(cn=${username})'
 export LDAP_ACCOUNTFULLNAME='${cn}'
 export LDAP_GROUPPATTERN='(cn=${groupname})'
 export LDAP_GROUPMEMBERPATTERN='(uniqueMember=${dn})'
-export LDAP_MANAGER_DN="cn=admin,dc=ldap,dc=example,dc=com"
+export LDAP_MANAGER_DN="cn=admin,dc=ldap,dc=xongl,dc=com"
 export LDAP_GROUP_NAME_ADMIN="administrators"
 
 # Gitlab Postgresql
@@ -25,7 +25,7 @@ export GITLAB_POSTGRES_DB="gitlabhq-production"
 # Gitlab and Jenkins
 
 export JENKINS_PLATFORM_USERNAME="jenkins"
-export GITLAB_PLATFORM_USERNAME="gitlab"
+export GITLAB_PLATFORM_USERNAME="saqsham"
 export GITLAB_JENKINS_USERNAME="jenkins"
 
 # Sonar MySQL
